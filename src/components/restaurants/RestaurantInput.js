59cc4fc43bf97ac05d2626fec390d846386b62c0
Rahman Elsirafy <rahman2408@gmail.com>
2021-12-15 14:26:06 -0500

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  // handleSubmit(e) {
+  //   e.preventDefault() ;
+  //   console.log(e.target.value)
+  // }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        Restaurant Input For New Business Goes Here :
+        < form onSubmit={this.handleSubmit()}>
+          <input type={"text"} placeholder='Restaurant Name'>Enter Business Name:</input>
+          <input type={"submit"}/>Submit Dine-In <input />
+
+          
+        </form>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,6 +1,9 @@
 import cuid from 'cuid';
+import Restaurant from '../components/restaurants/Restaurant';
 export const cuidFn = cuid;
- 
+ const manageRestaurants = (state = { restaurants: [], reviews: []}) => {
+   
+ }
 export default function manageRestaurants(state, action) {
 
 }
